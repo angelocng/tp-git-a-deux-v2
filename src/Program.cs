f2b869aa853f6a225faba50faf4622b350975088
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-08-22 14:14:23 +0200

    rectificatio finale etape3

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,8 @@
 using src;
 
 Console.WriteLine("Veuillez saisir votre nom et votre prenom svp.");
-Person personne1 = new Person();
-personne1.Firstname = Console.ReadLine();
-Console.WriteLine(personne1);
+string Firstname = Console.ReadLine();
+string Lastname = Console.ReadLine();
+Console.WriteLine(Firstname +" "+  Lastname);
 
 
