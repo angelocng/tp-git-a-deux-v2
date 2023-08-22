88b531ac0e2c94d94338639020990854739eeb1f
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-08-22 12:32:41 +0200

    modification etape 3

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Veuillez saisir votre nom et votre prenom svp.");
-personne1 = new Personne;
+personne1 = new Person;
 personne1 = Console.ReadLine();
 Console.WriteLine(personne1);
