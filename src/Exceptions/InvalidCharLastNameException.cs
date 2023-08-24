9b6f70e1750b7c70371e3668bce6ddac60cd4a4e
Johnny Charvet <43849966+JoCharvet@users.noreply.github.com>
2023-08-24 14:59:55 +0200

    add name Exception and CheckString

---
+++ src/Exceptions/InvalidCharLastNameException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Exception levée lorsqu'un caractère non authorisé est rencontré
+/// </summary>
+public class InvalidCharLastNameException : Exception { 
+
+	public InvalidCharLastNameException() : base("Charactère non authorisé dans le nom."){ }
+	
+	public InvalidCharLastNameException(string message) :base(message){ }
+}
