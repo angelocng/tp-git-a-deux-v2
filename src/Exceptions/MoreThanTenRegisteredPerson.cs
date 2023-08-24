6ce582f9afed41e8d3d1d8fd3672c6375d8c6874
FERRAND Nicolas <NFerrand@arfp.asso.fr>
2023-08-24 14:52:54 +0200

    Suppress PersonName Var from Exception Class

--- src/Exceptions/MoreThanTenRegisteredPerson.cs
+++ src/Exceptions/MoreThanTenRegisteredPerson.cs
@@ -3,7 +3,6 @@ namespace src
     [Serializable]
     public class MoreThanTenRegisteredPerson : Exception 
     { 
-        private string personName { get; set; }
         public MoreThanTenRegisteredPerson(){}
         public MoreThanTenRegisteredPerson(string message) : base (message)
         {
@@ -11,10 +10,5 @@ namespace src
         }
 
         public MoreThanTenRegisteredPerson(string message, Exception inner) : base (message, inner){}
-
-        public MoreThanTenRegisteredPerson(string message, string personName) : this (message)
-        {
-            this.personName = personName;
-        }
     }
 }
