0f361bbd12cfa32e4807201d4e1b51b6331b4db8
Rognone Gérard <65392789+GRognone@users.noreply.github.com>
2023-08-24 14:40:46 +0200

    Modification de la regex de verification des prénoms

--- src/Tools/BiblioRegex.cs
+++ src/Tools/BiblioRegex.cs
@@ -3,9 +3,9 @@ namespace src.Tools
 {
     public class Control
     {
-        public static bool ControlNameUser(string nameUser)
+        public static bool ControlFirstNameUser(string firstNameUser)
         {
-            return Regex.Match(nameUser, @"^[\p{L}\-]{1,40}$").Success;
+            return Regex.Match(firstNameUser, @"^[\p{L}\-]{1,40}$").Success;
         }
     }
 }
