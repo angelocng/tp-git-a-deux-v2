73cd65640dc41e580e3e4671143cd0e0839149da
acongiu <acongiu@CRM-UC-3915>
2023-08-24 12:06:44 +0200

    ticketConsoleLog

---
+++ src/Logger/ConsoleLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavGe.Logger
+{
+    public class ConsoleLogger : AbstractLogger
+    {
+        public override void LogMessage(string message)
+        {
+            this.Write(message);
+        }
+        internal override void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
