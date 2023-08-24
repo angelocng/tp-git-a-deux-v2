e91ec250a2c80e0b921f1bb819b0129910ce6ae9
FERRAND Nicolas <NFerrand@arfp.asso.fr>
2023-08-24 14:03:14 +0200

    Add control in PeopleContainer, can't get more than 10 person in list

--- src/Exceptions/MoreThanTenRegisteredPerson.cs
+++ src/Exceptions/MoreThanTenRegisteredPerson.cs
@@ -1,14 +1,20 @@
-[Serializable]
-public class MoreThanTenRegisteredPerson : Exception 
-{ 
-    private string PersonName {get;}
-    public MoreThanTenRegisteredPerson(){}
-    public MoreThanTenRegisteredPerson(string message) : base (message){}
+namespace src
+{
+    [Serializable]
+    public class MoreThanTenRegisteredPerson : Exception 
+    { 
+        private string PersonName {get;}
+        public MoreThanTenRegisteredPerson(){}
+        public MoreThanTenRegisteredPerson(string message) : base (message)
+        {
 
-    public MoreThanTenRegisteredPerson(string message, Exception inner) : base (message, inner){}
+        }
 
-    public MoreThanTenRegisteredPerson(string message, string personName) : this (message)
-    {
-        PersonName = personName;
+        public MoreThanTenRegisteredPerson(string message, Exception inner) : base (message, inner){}
+
+        public MoreThanTenRegisteredPerson(string message, string personName) : this (message)
+        {
+            PersonName = personName;
+        }
     }
 }
